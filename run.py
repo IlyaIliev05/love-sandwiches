9d2f9b483bfed3350557ac7dab77ef7bcce78c28
IlyaIliev05 <ilievilia15@gmail.com>
2022-07-28 20:00:46 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -42,13 +42,28 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f'Exactly 6 values required, you provided {len(values)}'
-            ) 
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
 
-    return True    
+    return True
+
+
+def update_sales_worksheet(data):
+    """ 
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 
 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
+
