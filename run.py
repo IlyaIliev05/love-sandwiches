8c82e34d3fed2fe8fe7d41d549eb00912f7ccbe8
IlyaIliev05 <ilievilia15@gmail.com>
2022-07-29 10:33:22 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -24,7 +24,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
@@ -51,6 +51,7 @@ def validate_data(values):
         return False
 
     return True
+
 
 def update_worksheet(data, worksheet):
     """
@@ -81,6 +82,7 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+
 def get_last_5_entries_sales():
     """
     Collects columns of data from sales worksheet, collecting
@@ -89,13 +91,13 @@ def get_last_5_entries_sales():
     """
     sales = SHEET.worksheet("sales")
     
-
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
     return columns  
-    
+
+
 def calculate_stock_data(data):
     """
     Calculate the average stock for each item type, adding 10%
@@ -110,7 +112,6 @@ def calculate_stock_data(data):
         new_stock_data.append(round(stock_num))
 
     return new_stock_data  
-
 
 
 def main():
